fb11f3988b718ee0fbd928ee25ed54bf651468bb
Richard Widjaya <ricwidjaya@gmail.com>
2022-08-24 20:25:53 +0800

    feat: add customer page

--- controllers/page/admin-controller.js
+++ controllers/page/admin-controller.js
@@ -8,6 +8,17 @@ const adminController = {
     } catch (error) {
       next(error)
     }
+  },
+
+  getCustomerPage: async (req, res, next) => {
+    try {
+      return res.render('admin/customer', {
+        layout: 'admin',
+        route: 'customer'
+      })
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
--- routes/page/modules/admin.js
+++ routes/page/modules/admin.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const adminController = require('../../../controllers/page/admin-controller')
 
 router.get('/order', adminController.getOrderPage)
+router.get('/customer', adminController.getCustomerPage)
 
 router.get('/', (req, res, next) => {
   return res.redirect('/admin/order')
